cbfd4d53d468df135125f4889add215a1d217a05
sebastian-meier <contact@sebastianmeier.eu>
2017-08-01 02:26:12 +0200

    finalising pipeline

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ var GIFEncoder = require('gifencoder');
 var fs = require('fs');
 var PNG = require('png-js');
 
-var encoder = new GIFEncoder(500, 250);
+var encoder = new GIFEncoder(600, 300);
 // stream the results as they are available into myanimated.gif
 encoder.createReadStream().pipe(fs.createWriteStream('myanimated.gif'));
 
 encoder.start();
 encoder.setRepeat(-1);   // 0 for repeat, -1 for no-repeat
 encoder.setDelay(1000/30);  // frame delay in ms
-encoder.setQuality(10); // image quality. 10 is default.
+encoder.setQuality(1); // image quality. 10 is default.
+//encoder.setTransparent(0xFFFFFF)
 
-var max = 150, c = 0;
+var max = 89, c = 0;
 
 function readFile(){
   fs.readFile('../SVIFT_render-image-module@bitbucket/snap/snap_'+c+'.png', function(err, data){
@@ -29,13 +30,3 @@ function readFile(){
 }
 
 readFile()
-
-/*var width  = myimage.width;
-var height = myimage.height;
-
-
-    //Pixels is a 1D array containing pixel data
-});*/
-
-
-
