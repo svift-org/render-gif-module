1d7a40ca6c4f326e784b014af36d667114e6ca84
sebastian-meier <contact@sebastianmeier.eu>
2017-08-23 01:41:22 +0200

    fixes

--- index.js
+++ index.js
@@ -8,9 +8,11 @@
 
 var GIFEncoder = require('gifencoder'),
   fs = require('fs'),
+  gif = require('gif'),
   PNG = require('png-js'),
   fast = require('fast-png'),
   upng = require('upng-js'),
+  gm = require('gm'),
   pngparse = require('pngparse')
 
 var render = (function () {
@@ -21,7 +23,8 @@ var render = (function () {
     files,
     count,
     render_folder,
-    png_file
+    png_file,
+    gmo
 
   /**
   * Initiate the rendering process by sending a data object containing params, vis and data (see data/example.json for structure)
@@ -34,18 +37,22 @@ var render = (function () {
     render_callback = callback
     render_folder = folder
 
-    encoder = new GIFEncoder(width, height);
+    //encoder = new GIFEncoder(width, height);
 
     var split = folder.split('/'),
       name = split[split.length-1]
 
-    encoder.createReadStream().pipe(fs.createWriteStream(folder + '/' + name + '.gif'));
+    png_file = folder + '/' + name + '.gif'
 
-    encoder.start();
-    encoder.setRepeat(-1);   // 0 for repeat, -1 for no-repeat
-    encoder.setDelay(1000/30);  // frame delay in ms
-    encoder.setQuality(1); // image quality. 10 is default.
+    //encoder.createReadStream().pipe(fs.createWriteStream(folder + '/' + name + '.gif'));
+
+    //encoder.start();
+    //encoder.setRepeat(-1);   // 0 for repeat, -1 for no-repeat
+    //encoder.setDelay(1000/30);  // frame delay in ms
+    //encoder.setQuality(1); // image quality. 10 is default.
     //encoder.setTransparent(0xFFFFFF)
+
+    gmo = Gm();
 
     count = 0
     files = fs.readdirSync(folder + '/png')
@@ -56,12 +63,16 @@ var render = (function () {
   module.addFiles = function(){
     if((files[count].split('.'))[1] == 'png'){
       console.log(render_folder+'/png/'+files[count])
-      fs.readFile(render_folder+'/png/'+files[count], function(err, data){
+
+      gmo.in(render_folder+'/png/'+files[count])
+      module.nextFile()
+
+      /*fs.readFile(render_folder+'/png/'+files[count], function(err, data){
         //png_file = new PNG(data);
         /*png_file.decode(function (pixels) {
           encoder.addFrame(pixels)
           module.nextFile()
-        })*/
+        })
         //png_file = fast.decode(data)
         //encoder.addFrame(png_file)
 
@@ -94,7 +105,16 @@ var render = (function () {
     if(count<files.length){
       module.addFiles()
     }else{
-      encoder.finish()
+      //encoder.finish()
+
+      gmo
+        .delay(100)
+        .resize(500,500)
+        .write(png_file, function(err){
+          if (err) throw err;
+          console.log("animated.gif created");
+        });
+
       render_callback()
     }
   }
