a2e128fec250df2be38629ad0e63d88e6235617d
sebastian-meier <contact@sebastianmeier.eu>
2017-08-22 19:23:02 +0200

    basic functionality

--- index.js
+++ index.js
@@ -1,32 +1,82 @@
-var GIFEncoder = require('gifencoder');
-var fs = require('fs');
-var PNG = require('png-js');
+/*
+ *
+ * Add header info here
+ *
+ */
 
-var encoder = new GIFEncoder(600, 300);
-// stream the results as they are available into myanimated.gif
-encoder.createReadStream().pipe(fs.createWriteStream('myanimated.gif'));
+'use strict';
 
-encoder.start();
-encoder.setRepeat(-1);   // 0 for repeat, -1 for no-repeat
-encoder.setDelay(1000/30);  // frame delay in ms
-encoder.setQuality(1); // image quality. 10 is default.
-//encoder.setTransparent(0xFFFFFF)
+var GIFEncoder = require('gifencoder'),
+  fs = require('fs'),
+  PNG = require('png-js')
 
-var max = 89, c = 0;
+var render = (function () {
+ 
+  var module = {},
+    encoder,
+    render_callback,
+    files,
+    count,
+    render_folder
 
-function readFile(){
-  fs.readFile('../SVIFT_render-image-module@bitbucket/snap/snap_'+c+'.png', function(err, data){
-    var myimage = new PNG(data);
-    myimage.decode(function (pixels) {
-      encoder.addFrame(pixels);
-      c++;
-      if(c<max){
-        readFile();
-      }else{
-        encoder.finish();
-      }
-    })
-  })
-}
+  /**
+  * Initiate the rendering process by sending a data object containing params, vis and data (see data/example.json for structure)
+  *
+  * @param {Object} `data` Parameters for the rendering process.
+  * @api public
+  */
 
-readFile()
+  module.render = function (folder, width, height, callback) {
+    render_callback = callback
+    render_folder = folder
+
+    encoder = new GIFEncoder(width, height);
+
+    var split = folder.split('/'),
+      name = split[split.length-1]
+
+    encoder.createReadStream().pipe(fs.createWriteStream(folder + '/' + name + '.gif'));
+
+    encoder.start();
+    encoder.setRepeat(-1);   // 0 for repeat, -1 for no-repeat
+    encoder.setDelay(1000/30);  // frame delay in ms
+    encoder.setQuality(1); // image quality. 10 is default.
+    //encoder.setTransparent(0xFFFFFF)
+
+    count = 0
+    files = fs.readdirSync(folder + '/png')
+
+    module.addFiles()
+  }
+
+  module.addFiles = function(){
+    if((files[count].split('.'))[1] == 'png'){
+
+      fs.readFile(render_folder+'/gif/'+files[count], function(err, data){
+        var myimage = new PNG(data);
+          myimage.decode(function (pixels) {
+            encoder.addFrame(pixels)
+            module.nextFile()
+          })
+      })
+
+    }else{
+      module.nextFile()
+    }
+  }
+
+  module.nextFile = function(){
+    count++
+    if(count<files.length){
+      module.addFiles()
+    }else{
+      encoder.finish()
+      render_callback()
+    }
+  }
+
+  return module;
+ 
+})();
+
+module.exports = render;
