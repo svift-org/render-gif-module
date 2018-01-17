7c5bdee7374065e2e46033f58f63946c626e6c9a
sebastian-meier <contact@sebastianmeier.eu>
2018-01-17 02:02:41 +0100

    speed test

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ var render = (function () {
       name = split[split.length-1]
 
     fs.readdir(folder+'/png/', (err, items) => {
-      im.convert(['-limit', 'memory', '128mb', '-loop', 1, '-delay', 100/30, folder+'/png/*.png', '-delay', 250, folder+'/png/'+items[items.length-1], folder + '/' + name + '.gif'], function (err, stdout) {
+      //'-limit', 'memory', '128mb', 
+      im.convert(['-loop', 1, '-delay', 100/30, folder+'/png/*.png', '-delay', 250, folder+'/png/'+items[items.length-1], folder + '/' + name + '.gif'], function (err, stdout) {
         if (err) throw err;
         render_callback()
       });
